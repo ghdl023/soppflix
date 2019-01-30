c9e6de7ce5a3b4dcba34318d0feab2dfea84d8e2
gwee1216 <gwee1216@naver.com>
2019-01-30 10:47:23 +0900

    #3.3 GlobalStyles and Header
    npm i styled-reset
    
    import { createGlobalStyle} from "styled-components";

---
+++ src/Components/GlobalStyles.js
@@ -0,0 +1,21 @@
+import reset from "styled-reset";
+import { createGlobalStyle } from "styled-components";
+
+const globalStyles = createGlobalStyle`
+    ${reset};
+    a{
+        text-decoration:none;
+        color:white;
+    }
+    *{
+        box-sizing:border-box;
+    }
+    body{
+        /* font-size:12px; */
+        background-color:rgba(20,20,20,1);
+        color:white;
+        padding-top:50px;
+    }
+`;
+
+export default globalStyles;
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,18 +2,36 @@ import React from "react";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
 
-const Header = styled.header``;
+const Header = styled.header`
+  position: fixed;
+  top: 0px;
+  left: 0px;
+  display: flex;
+  align-items: center;
+  height: 50px;
+  width: 100%;
+
+  z-index: 15;
+  background-color: rgba(25, 25, 25, 0.7);
+  box-shadow: 0px 1px 5px 2px rgba(0, 0, 0, 0.5);
+`;
 
 const List = styled.ul`
   display: flex;
-  &:hover {
-    background-color: dodgerblue;
-  }
 `;
 
-const Item = styled.li``;
+const Item = styled.li`
+  width: 80px;
+  height: 50px;
+  text-align: center;
+`;
 
-const SLink = styled(Link)``;
+const SLink = styled(Link)`
+  height: 50px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
 
 export default () => (
   <Header>
