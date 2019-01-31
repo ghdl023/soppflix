e2f9f490b55891ebdb48760bcbd40150fa376212
gwee1216 <gwee1216@naver.com>
2019-01-31 15:42:22 +0900

    #5.2 Home Container

--- src/Routes/Home/HomeContainer.js
+++ src/Routes/Home/HomeContainer.js
@@ -1,5 +1,6 @@
 import React from "react";
 import HomePresenter from "./HomePresenter";
+import { moviesApi } from "api";
 
 export default class extends React.Component {
   state = {
@@ -10,10 +11,54 @@ export default class extends React.Component {
     error: null
   };
 
+  async componentDidMount() {
+    try {
+      const nowPlaying = await this._getNowPlaying();
+      const upcoming = await this._getUpComing();
+      const popular = await this._getPopular();
+      // throw Error();
+      this.setState({
+        nowPlaying,
+        upcoming,
+        popular
+      });
+    } catch (error) {
+      this.setState({
+        error: "Can't find Moives Information..."
+      });
+    } finally {
+      this.setState({
+        loading: false
+      });
+    }
+  }
+
   // logic
+
+  _getNowPlaying = async () => {
+    const {
+      data: { results }
+    } = await moviesApi.nowPlaying();
+    return results;
+  };
+
+  _getUpComing = async () => {
+    const {
+      data: { results }
+    } = await moviesApi.upcoming();
+    return results;
+  };
+
+  _getPopular = async () => {
+    const {
+      data: { results }
+    } = await moviesApi.popular();
+    return results;
+  };
 
   render() {
     const { nowPlaying, upcoming, popular, loading, error } = this.state;
+    console.log(this.state);
     return (
       <HomePresenter
         nowPlaying={nowPlaying}
