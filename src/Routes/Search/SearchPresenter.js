113ab195645455a7bdd2aea8c5dcbbb365692ae0
gwee1216 <gwee1216@naver.com>
2019-02-01 23:06:06 +0900

    #6.3 SearchPresenter Component
    input값(searchTerm)이 Container의 상태값(state)에 ""로 초기화 되어있다.
    
    Submit Form 할때마다 input값(searchTerm)이 API의 파라미터로 이용되는데 ""로 초기화되있는값을 바꿔주지 않으면 당연히 안됨.
    
    그렇기때문에 input에 onChange 이벤트를 주고, 그 이벤트에서는 input의 current value를 state로 업데이트 시켜줘야만한다!

--- src/Routes/Search/SearchPresenter.js
+++ src/Routes/Search/SearchPresenter.js
@@ -1,6 +1,28 @@
 import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
+import Loader from "../../Components/Loader";
+import Section from "../../Components/Section";
+
+const Container = styled.div`
+  padding: 0 10px;
+`;
+
+const Form = styled.form`
+  margin-bottom: 50px;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+`;
+
+const Input = styled.input`
+  all: unset;
+  text-align: center;
+  width: 40%;
+  font-size: 45px;
+  border-bottom: 1px solid rgba(255, 255, 255, 0.5);
+  padding: 10px 0px;
+`;
 
 const SearchPresenter = ({
   movieResults,
@@ -8,16 +30,48 @@ const SearchPresenter = ({
   searchTerm,
   loading,
   error,
-  handleSubmit
-}) => null;
+  handleSubmit,
+  updateTerm
+}) => (
+  <Container>
+    <Form onSubmit={handleSubmit}>
+      <Input
+        placeholder="Search Movies or TV Shows..."
+        value={searchTerm}
+        onChange={updateTerm}
+      />
+    </Form>
+    {loading ? (
+      <Loader />
+    ) : (
+      <>
+        {movieResults && movieResults.length > 0 && (
+          <Section title="Movie Results">
+            {movieResults.map(movie => (
+              <span key={movie.id}>{movie.title}</span>
+            ))}
+          </Section>
+        )}
+        {tvResults && tvResults.length > 0 && (
+          <Section title="TV Show Results">
+            {tvResults.map(show => (
+              <span key={show.id}>{show.name}</span>
+            ))}
+          </Section>
+        )}
+      </>
+    )}
+  </Container>
+);
 
 SearchPresenter.propTypes = {
   movieResults: PropTypes.array,
   tvResults: PropTypes.array,
-  searchTerm: PropTypes.array,
+  searchTerm: PropTypes.string,
   loading: PropTypes.bool.isRequired,
   error: PropTypes.string,
-  handleSubmit: PropTypes.func.isRequired
+  handleSubmit: PropTypes.func.isRequired,
+  updateTerm: PropTypes.func.isRequired
 };
 
 export default SearchPresenter;
