114661785dcbc903a39201436d6fc61ccc7db296
gwee1216 <gwee1216@naver.com>
2019-02-01 21:15:29 +0900

    #6.0 Presenter Structure

--- src/Routes/Detail/DetailPresenter.js
+++ src/Routes/Detail/DetailPresenter.js
@@ -1,1 +1,13 @@
-export default () => "Detail";
+import React from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components";
+
+const DetailPresenter = ({ result, loading, error }) => null;
+
+DetailPresenter.PropTypes = {
+  result: PropTypes.array,
+  loading: PropTypes.boolean.isRequired,
+  error: PropTypes.string
+};
+
+export default DetailPresenter;
